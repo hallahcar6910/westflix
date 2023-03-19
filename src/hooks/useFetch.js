590cd9b8f35f654598475f1336e04b5a83701af2
hallahcar6910 <hallahcar@gmail.com>
2023-03-19 07:31:51 +0100

    Key changed

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 export const useFetch = (apiPath, queryTerm="") => {
     const [data, setData] = useState([]);
-    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${83103f979001bf9d0e64389f9a75b031}&query=${queryTerm}`
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=83103f979001bf9d0e64389f9a75b031${queryTerm}`
     // const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`
 
 
