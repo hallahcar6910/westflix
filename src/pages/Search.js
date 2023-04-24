12d2333024176fb642b9589873d24ee445009346
hallahcar6910 <hallahcar@gmail.com>
2023-04-24 17:13:24 +0100

    main search done

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -13,7 +13,7 @@ useTitle(`Search result for ${queryTerm}`);
   return (
     <main>
       <section className="py-7">
-        <p className="text-3xl text-gray-700 dark:text-white">{ movies.length === 0 ? `No result found for '${queryTerm}'` : `Result for '${queryTerm}'` }</p>
+        <p className="text-3xl text-gray-700 dark:text-white">{ movies.length === 0 ? `No Result Found Fgit or '${queryTerm}'` : `Result for '${queryTerm}'` }</p>
       </section>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">       
